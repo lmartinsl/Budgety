10dbda52b6296f8cd49949c1110d43b71984629e
Lucas Lima <lmartinsl@indracompany.com>
2019-01-24 12:34:55 -0200

    Atualizando o Orçamento: UI Controller

--- app.js
+++ app.js
@@ -82,12 +82,12 @@ var budgetController = (function() {
             }
         },
 
-        getBudget: function() {
+        getBudget: function() { // será retornado ao controlador do app que transmite para o método displayBudget
             return {
-                budget      : data.budget,
-                totalInc    : data.totals.inc,
-                totalExp	: data.totals.exp,
-                percentage  : data.percentage
+                budget      : data.budget,      // total
+                totalInc    : data.totals.inc,  // total receitas
+                totalExp	: data.totals.exp,  // total despesas
+                percentage  : data.percentage	// porcentagem
             }
         },
         
@@ -101,14 +101,18 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
     
+    // Armazenameno de classes names
     var DOMstrings = {
-        inputType	        : '.add__type',
-        inputDescription	: '.add__description',
-        inputValue	        : '.add__value',
-        inputBtn	        : '.add__btn',
-        incomeContainer	    : '.income__list',
-        expensesContainer	: '.expenses__list',
-        
+        inputType	        : '.add__type',                     // tipo de aplicação - receita(income)/despesas(expenses)
+        inputDescription	: '.add__description',              // descrição
+        inputValue	        : '.add__value',                    // valor
+        inputBtn	        : '.add__btn',                      // botão para enviar dados
+        incomeContainer	    : '.income__list',                  // div que separa as receitas
+        expensesContainer	: '.expenses__list',                // div que separa as despesas
+        budgetLabel	        : '.budget__value',                 // valor total
+        incomeLabel	        : '.budget__income--value',         // total receitas
+        expensesLabel	    : '.budget__expenses--value',       // total despesas
+        percentageLabel	    : '.budget__expenses--percentage'   // porcentagem
     };        
     
     return {
@@ -145,7 +149,7 @@ var UIController = (function() {
             // Insert the HTML into the DOM
             // DOM into html with income__container or expenses__container
                                                               //posição   //texto
-            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml); // insere no DOM o new html dentro da propriedade element
         },
 
         clearFields: function() {
@@ -161,6 +165,19 @@ var UIController = (function() {
             });
 
             fieldsArr[0].focus();
+        },
+
+        displayBudget: function(obj) {
+
+                document.querySelector(DOMstrings.budgetLabel).textContent      = obj.budget;
+                document.querySelector(DOMstrings.incomeLabel).textContent      = obj.totalInc;
+                document.querySelector(DOMstrings.expensesLabel).textContent    = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent  = '- ' + obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent  = '---';
+            }
         },
 
         getDOMstrings: function() { // Expõe o DOMstrings para funções externas 
@@ -194,7 +211,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
+        //console.log(budget);
 
     };
     
@@ -231,6 +249,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget      : 0,
+                totalInc    : 0,
+                totalExp	: 0,
+                percentage  : -1
+            });
             setupEventListeners();
         }
     };
